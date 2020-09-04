77c5e8e26df7bb57a3b4db50a5c98171cc67aa89
2shady4u <jd16581556@gmail.com>
2020-09-04 20:54:00 +0200

    Add pre-commit scripts in PS

--- .githooks/pre-commit.ps1
+++ .githooks/pre-commit.ps1
@@ -1,25 +1,66 @@
 write-output "This is a pre-commit powershell call"
 write-output "======================================="
 
-Import-Module -Name PSScriptAnalyzer
+# Set path correctly.
+$originaldir = Get-Location
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$dir = Split-Path -Path $dir -Parent
 
-$changes = git diff –name-only
-$output = @()
+Write-host "My directory is $dir"
+Set-Location -Path $dir
 
-foreach ($change in $changes)
+$exfiles = @('README.md', 'pre-commit', 'pre-commit.ps1')
+$exdirectories = @('.githooks')
+
+$files = Get-ChildItem -Path $dir -Recurse -Name -File
+$directories = Get-ChildItem -Path $dir -Recurse -Name -Directory
+$matchstring = '^[a-z][_[a-z]+]?$'
+$output = 0
+
+foreach ($name in $files)
 {
-    write-output "Running ScriptAnalyzer against: $change"
-    $winPath = $change.replace("/", "\")
-    $winPath = ".\$winPath"
-    $out = Invoke-ScriptAnalyzer -Path $winPath
-    $output += $out
+    if ($exfiles -contains (Get-Item $name ).Name){
+        continue
+    }
+
+    $name = (Get-Item $name ).BaseName
+    $snakematch = $name -cmatch $matchstring
+    if ($snakematch) {
+        write-output "Snake matched: $name"
+    } else {
+        write-output "Does not obey snake_match: $name"
+        $output += 1
+    }
+}
+
+foreach ($name in $directories)
+{
+    if ($exdirectories -contains $name){
+        continue
+    }
+
+    $name = (Get-Item $name ).BaseName
+    $snakematch = $name -cmatch $matchstring
+    if ($snakematch) {
+        write-output "Snake matched: $name"
+    } else {
+        write-output "Does not obey snake_match: $name"
+        $output += 1
+    }
 }
 
 write-output "======================================="
 
-if ($output.Count -ne 0)
+# Go back to the root folder.
+Set-Location -Path $originaldir
+
+if ($output -ne 0)
 {
-    Write-Output "Basic scripting errors were found in updated scripts. fix or use git commit –no-verify"
+    Write-Output "Naming conventions are not respected! Use snake_case for all file- and folder-names!"
     $output.Message
     exit 1
+} else {
+    Write-Output "All naming conventions seem to be in order... you win this one..."
+    exit 0
 }
